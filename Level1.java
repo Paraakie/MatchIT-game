71df2c59f08bbdbe4e9cf315b5f4bd1ca02122fd
Paraakie <sven.m.gerhards@gmail.com>
2018-01-10 12:34:45 +1300

    Update and rename Level1.java to MatchIT.java

--- Level1.java
+++
@@ -1,6 +0,0 @@
-
-public class Level1 extends GameEngine{
-	public static void main(String agrs[]) {
-		createGame(new Level1());
-	}
-}
---
+++ MatchIT.java
@@ -0,0 +1,6 @@
+
+public class MatchIT. extends GameEngine{
+	public static void main(String agrs[]) {
+		createGame(new MatchIT.());
+	}
+}

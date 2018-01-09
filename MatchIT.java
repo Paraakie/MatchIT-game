6b950dbfbb6b7d679defb0d0d3553ea34516ae64
Paraakie <sven.m.gerhards@gmail.com>
2018-01-10 12:42:15 +1300

    Update MatchIT.java

--- MatchIT.java
+++ MatchIT.java
@@ -1,6 +1,31 @@
 
-public class MatchIT. extends GameEngine{
+public class MatchIT extends GameEngine{
 	public static void main(String agrs[]) {
-		createGame(new MatchIT.());
+		createGame(new MatchIT());
+	}
+	
+	/* Global string to switch between levels/menu/winScreen
+	 * Menu =  Main-menu
+	 * lvl1 = First level
+	 * score = Final score screen
+	 */
+	String currentLevel;
+	
+	@Override
+	public void init() {
+		//Player starts in the menu
+		currentLevel = "menu";
+	}
+	
+	@Override
+	public void update(double dt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void paintComponent() {
+		// TODO Auto-generated method stub
+		
 	}
 }

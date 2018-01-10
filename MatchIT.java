a56060a5f2f24837689c80a2d959a9b17acd8c4e
Paraakie <sven.m.gerhards@gmail.com>
2018-01-10 13:29:10 +1300

    Update MatchIT.java

--- MatchIT.java
+++ MatchIT.java
@@ -15,19 +15,37 @@ public class MatchIT extends GameEngine{
 	 * Main Menu
 	 */
 	
+	public void updateMenu() {
+		
+	}
 	
+	public void drawMenu() {
+		
+	}
 	
 	/*
 	 * Level 1
 	 */
 	
+	public void updateLevel1() {
+		
+	}
 	
+	public void drawLevel1() {
+		
+	}
 	
 	/*
 	 * Final Score Screen
 	 */
 	
+	public void updateScoreScreen() {
+		//Usesful?
+	}
 	
+	public void drawScoreScreen() {
+		
+	}
 	
 	// Initialise all global values here
 	@Override
@@ -46,7 +64,22 @@ public class MatchIT extends GameEngine{
 	// Main Draw Function, for graphical content
 	@Override
 	public void paintComponent() {
-		// TODO Auto-generated method stub
+		if(currentLevel == "menu") {
+			updateMenu();
+		
+		} else if(currentLevel == "lvl1") {
+			updateLevel1();
+		
+		} else if(currentLevel == "score") {
+			updateScoreScreen();
+		
+		} else {
+			/* This will only happen if currentLevel was changed to a wrong value
+			 * Thus this an error
+			 */
+			
+		}
+		
 		
 	}
 }
